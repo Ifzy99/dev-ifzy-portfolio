e9581ab329d30bdab3813786b7eedbaaee96d2af
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-09-26 20:54:35 +0100

    "Remove className from footer div element"

--- Components/Footer.js
+++ Components/Footer.js
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <>
-    <div id="footer" className="section-container text-darkGrayishBlue">
+    <div id="footer">
     <div className="flex flex-col text-center px-5 py-6 items-center md:flex-row md:justify-between md:text-sm ">
       <div className="order-2">
         Built and Designed by Olatunbosun Ifeoluwa | Powered by Next.js and

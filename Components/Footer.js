85c02fd31638084697b837b9ddd5053f7a57157f
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-09-30 19:35:58 +0100

    "Uncommented and rearranged Footer component code, and updated globals.css by uncommenting and removing some lines."

--- Components/Footer.js
+++ Components/Footer.js
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <>
-   {/* <div id="footer">
+   <div id="footer">
     <div className="flex flex-col text-center px-5 py-6 items-center md:flex-row md:justify-between md:text-sm ">
       <div className="order-2">
         Built and Designed by Olatunbosun Ifeoluwa | Powered by Next.js and
@@ -11,8 +11,8 @@ const Footer = () => {
       </div>
       <div className="">&copy; 2024 IFZY. All Rights Reserved</div>
     </div>
-  </div>*/}
-  <div id="footer">
+  </div>
+  {/*<div id="footer">
   <div class="footer-container">
     <div class="footer-text">
       Built and Designed by Olatunbosun Ifeoluwa | Powered by Next.js and Tailwind CSS
@@ -21,7 +21,7 @@ const Footer = () => {
       &copy; 2024 IFZY. All Rights Reserved
     </div>
   </div>
-</div>
+</div>*/}
 
     </>
   )
---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.3/dist/tailwind.min.css" rel="stylesheet"/>
+
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap&family=Cairo+Play:wght@200..1000&display=swap"
+            rel="stylesheet"
+          />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <link 
+            rel="stylesheet" 
+            href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" 
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
